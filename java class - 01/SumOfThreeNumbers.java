92ce07889408331b77a497e9409fc444a6e8a09d
Abhishek <abhishek.pvs5100@gmail.com>
2022-01-29 12:27:57 +0530

    Completed class - 01

---
+++ java class - 01/SumOfThreeNumbers.java
@@ -0,0 +1,12 @@
+public class SumOfThreeNumbers {
+
+    public static void main(String[] args) {
+        System.out.print("The Sum of the three number is " );
+        int num1 = 6;
+        int num2 = 7;
+        int num3 = 8;
+
+        System.out.println(num1 + num2 + num3);
+
+    }
+}
