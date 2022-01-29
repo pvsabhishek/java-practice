56846cca5a239d89bf7187576701d78220cfd84f
Abhishek <abhishek.pvs5100@gmail.com>
2022-01-29 13:50:14 +0530

    Completed Class - 04

---
+++ java class - 03 ( Input Class - Scanner )/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int int_Number = sc.nextInt();
+        System.out.println(int_Number);
+
+        float float_Number = sc.nextFloat();
+        System.out.println(float_Number);
+
+        boolean b1 = sc.hasNextInt();
+        System.out.println(b1);
+
+        String str = sc.next();
+        System.out.println(str);
+        
+    }
+
+}
